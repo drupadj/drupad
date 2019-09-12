04d83e998daaba6d3448b66a08f25901626820a5
drupadjoshi <drupadjoshi64@gmail.com>
2019-09-12 15:35:29 +0530

    YUV

---
+++ C/Geekforgeeks/Bitwise/IMP/0_1_Pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main () {
+
+int pos = 0, num = 0, count = 0;
+
+printf ("Enter a number : ");
+scanf ("%d", &num);
+
+for ( pos = 31; pos >= 0; pos--) {
+
+if ((!(num & 1 << pos)) && (num & 1 << pos - 1))
+		count++; 
+} 
+	printf ("Count is = %d\n", count);
+	return 0;
+}
